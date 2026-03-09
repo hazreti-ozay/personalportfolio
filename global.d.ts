bb8b9a05099bd1eaa5ddb467887992c35bce6088
hazreti-ozay <ozayozkan101@gmail.com>
2026-03-09 03:55:30 +0300

    CSS modül tür bildirimleri eklendi; tsconfig.json'dan "baseUrl" seçeneği kaldırıldı; next.config.ts için açıklama güncellendi

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+// CSS module type declarations
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Turbopack için boş config (Next.js 16 uyarısını kapatır)
+  turbopack: {},
 };
 
 export default nextConfig;
