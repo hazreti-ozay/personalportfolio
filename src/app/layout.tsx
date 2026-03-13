3a56b82859907db68f042a8e1221b1bf403fc957
hazreti-ozay <ozayozkan101@gmail.com>
2026-03-14 00:10:37 +0300

    @vercel/analytics ve @vercel/speed-insights paketleri eklendi; layout.tsx dosyasında entegrasyon sağlandı.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"; // Fontu çağırdım
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 // Font ayarı
@@ -18,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="tr" className="scroll-smooth">
       {/* Font sınıfını body'e veriyoruz */}
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
